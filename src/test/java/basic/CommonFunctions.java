625c8f1f0b61995305446caef5e64034e3cd33b7
hira84chirag <contactchiragpatel2022@gmail.com>
2025-07-23 22:56:54 +0530

    Stable version commited

--- src/test/java/basic/CommonFunctions.java
+++ src/test/java/basic/CommonFunctions.java
@@ -1,4 +1,4 @@
-package Util;
+package basic;
 
 import java.awt.AWTException;
 import java.awt.Rectangle;
--- src/test/java/basic/Flipkart.java
+++ src/test/java/basic/Flipkart.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
-import Util.CommonFunctions;
 
 
 public class Flipkart extends BaseTest{
